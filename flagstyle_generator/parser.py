567530407101e559ac215f3a422c81bf9855a267
Jesse <jesse@jessejanssen.nl>
2024-02-07 00:54:28 +0100

    Update parser.py

--- flagstyle_generator/parser.py
+++ flagstyle_generator/parser.py
@@ -381,6 +381,8 @@ class Parser:
 for raw_proposition in (
     '(P ⇒ (Q ∧ R)) ⇒ ((P ⇒ Q) ∧ (Q ⇒ (P ⇒ R)))',
     'a|b|c|d|e|f|g',
+    'a=>b=>c',
+    'a=>b=>c=>d',
 ):
     parser = Parser.from_raw_proposition(raw_proposition)
     parser.brute_force()
