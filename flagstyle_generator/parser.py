0f82935d9f52161bb8df26ebac50a2ae3a3b0d7f
Jesse <jesse@jessejanssen.nl>
2024-02-07 00:09:43 +0100

    Update parser.py

--- flagstyle_generator/parser.py
+++ flagstyle_generator/parser.py
@@ -224,6 +224,8 @@ class Parser:
     proposition: Token
     variables: dict[str, Variable]
     proposition_type: PropositionType
+    does_hold_for: list[dict[Variable, bool]]
+    does_not_hold_for: list[dict[Variable, bool]]
     def __init__(
         self,
         *,
@@ -233,6 +235,8 @@ class Parser:
         self.proposition = proposition
         self.variables = variables
         self.proposition_type = PropositionType.not_sure
+        self.does_hold_for = []
+        self.does_not_hold_for = []
 
     @staticmethod
     def maybe_add_variable(
@@ -346,15 +350,39 @@ class Parser:
         return cls(proposition=proposition, variables=variables)
 
     def brute_force(self) -> None:
+        self.does_hold_for = []
+        self.does_not_hold_for = []
         for n in range(2 ** len(self.variables)):
             for i, variable in enumerate(self.variables.values()):
                 variable.value = bool(n & (1 << i))
-            print(self.proposition, self.proposition.get_value())
+            holds: bool = self.proposition.get_value()
+            if holds:
+                self.does_hold_for.append({variable: variable.value for variable in self.variables.values()})
+            else:
+                self.does_not_hold_for.append({variable: variable.value for variable in self.variables.values()})
+        if len(self.does_not_hold_for) == 0:
+            self.proposition_type = PropositionType.tautology
+        elif len(self.does_hold_for) == 0:
+            self.proposition_type = PropositionType.contradiction
+        else:
+            self.proposition_type = PropositionType.contingency
+
+    def get_result_string(self) -> str:
+        if self.proposition_type is PropositionType.tautology:
+            return 'Proposition is a tautology.'
+        if self.proposition_type is PropositionType.contradiction:
+            return 'Proposition is a contradiction.'
+        return 'Proposition is a contingency. It does not hold for:\n' + '\n'.join(
+            f'    {', '.join(f'{variable.name}={int(value)}' for variable, value in variables.items())}'
+            for variables in self.does_not_hold_for
+        )
 
 
 for proposition in (
     '(P ⇒ (Q ∧ R)) ⇒ ((P ⇒ Q) ∧ (Q ⇒ (P ⇒ R)))',
+    'a|b|c|d|e|f|g',
 ):
     parser = Parser.from_raw_proposition(proposition)
-    print(parser.proposition)
     parser.brute_force()
+    print(parser.proposition)
+    print(parser.get_result_string())
