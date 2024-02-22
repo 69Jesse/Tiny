5da7e2b62dfca40964511c2f564d06a243218f88
Jesse <jesse@jessejanssen.nl>
2024-02-22 02:38:28 +0100

    Update main.py

--- flagstyle_generator/main.py
+++ flagstyle_generator/main.py
@@ -4,11 +4,12 @@ from generator import Generator
 def main() -> None:
     for raw_proposition in (
         '(P ⇒ (Q ∧ R)) ⇒ ((P ⇒ Q) ∧ (Q ⇒ (P ⇒ R)))',
-        'a|b|c|d|e|f|g',
-        'a=>b=>c',
-        'a=>b=>c=>d',
+        # 'a|b|c|d|e|f|g',
+        # 'a=>b=>c',
+        # 'a=>b=>c=>d',
     ):
         generator = Generator.from_proposition(raw_proposition)
+        print(generator.parser.proposition)
         generator.check(display=True)
 
 
