81a0e20d48faccb05f34ba78dbaba1318dc33934
Jesse <jesse@jessejanssen.nl>
2024-03-29 03:06:29 +0100

    Update add_newlines.py

--- add_newlines.py
+++ add_newlines.py
@@ -1,28 +1,55 @@
+from typing import Optional
+
 
 def add_newlines(
-    message: str,
-    *,
-    max_line_size: int,
+	text: str,
+	*,
+	max_line_length: Optional[int] = None,
+	max_length_ratio: float = 4 / 1,
+	min_line_length: int = 20,
+	max_length_margin: int = 4,
+	ignore_newlines: bool = True,
 ) -> str:
-    lines: list[str] = []
-    line: str = ''
-    for word in message.split(' '):
-        if not line:
-            new = word
-        else:
-            new = line + ' ' + word
-        if len(new) > max_line_size:
-            if line:
-                lines.append(line)
-            if len(word) > max_line_size:
-                div, mod = divmod(len(word), max_line_size)
-                for _ in range(div + (mod != 0)):
-                    part = word[:max_line_size]
-                    lines.append(part)
-                    word = word[max_line_size:]
-            line = word
-            continue
-        line = new
-    if line:
-        lines.append(line)
-    return '\n'.join(lines)
+	if ignore_newlines:
+		text = text.replace('\n', ' ')
+	if max_line_length is not None:
+		line_length = max_line_length
+	else:
+		line_length = round((
+			(len(text) / max_length_ratio) ** 0.5
+		) * max_length_ratio)
+	line_length = max(line_length, min_line_length)
+
+	lines: list[str] = []
+	for left in text.split('\n'):
+		line: str = ''
+		words: list[str] = left.split(' ')
+		while len(words) > 0:
+			before = len(line)
+			word = words.pop(0).strip(' ')
+			new_line = (line + ' ' + word).removeprefix(' ')
+			after = len(new_line)
+			before_diff, after_diff = line_length - before, line_length - after
+			if after_diff > 0:
+				line = new_line
+				continue
+			abs_after_diff = abs(after_diff)
+			if abs_after_diff <= max_length_margin:
+				if abs(before_diff) < abs_after_diff:
+					lines.append(line)
+					line = word
+					continue
+				lines.append(new_line)
+				line = ''
+				continue
+			if len(line) == 0:
+				left, right = word[:line_length], word[line_length:]
+				lines.append(left)
+				words.insert(0, right)
+				line = ''
+				continue
+			lines.append(line)
+			line = word
+		if len(line) > 0:
+			lines.append(line)
+	return '\n'.join(lines)
