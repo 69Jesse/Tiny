99fb80fea10a6a4abdbe0c031aa23b9cacbb1e56
Jesse <jesse@jessejanssen.nl>
2024-04-21 20:17:38 +0200

    Update everything.py

--- using_pyhtsl/rpg idk/everything.py
+++ using_pyhtsl/rpg idk/everything.py
@@ -2,8 +2,10 @@ from pyhtsl import (
     Team,
     GlobalStat,
     PlayerStat,
+    Item,
 )
 
+from enum import Enum, auto
 import re
 
 from typing import Optional
@@ -28,7 +30,7 @@ LINE_REGEX = re.compile(r'^(x+)(\.*)$')
 def fetch_digits(
     fetch_from: PlayerStat,
     assign_to: PlayerStat,
-    line: Optional[str] = None,
+    line: Optional[str] = None,  # 'xxx...' means number abcdefghi -> def
     *,
     size: Optional[int] = None,
     right_offset: Optional[int] = None,
@@ -42,3 +44,32 @@ def fetch_digits(
         right_offset = len(match.group(2))
     high, low = 10 ** (size + right_offset), 10 ** right_offset
     assign_to.value = (fetch_from - (fetch_from // high * high)) // low
+
+
+class ItemRarity(Enum):
+    COMMON = 'f'
+    UNCOMMON = 'a'
+    RARE = '9'
+    EPIC = '5'
+    LEGENDARY = '6'
+    MYTHIC = 'd'
+
+
+class ItemType(Enum):
+    Armor = auto()
+    Weapon = auto()
+    Tool = auto()
+    Consumable = auto()
+    Material = auto()
+
+
+class CustomItem:
+    name: str
+    rarity: ItemRarity
+    type: ItemType
+
+    @property
+    def item(self) -> None:
+        return Item(
+            
+        )
