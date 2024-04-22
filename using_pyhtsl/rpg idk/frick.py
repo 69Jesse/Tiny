3902b9d2f3b965444d58ca79f309fbc25d8ac290
Jesse <jesse@jessejanssen.nl>
2024-04-22 12:16:19 +0200

    Update frick.py

--- using_pyhtsl/rpg idk/frick.py
+++ using_pyhtsl/rpg idk/frick.py
@@ -1,25 +1,10 @@
-from pyhtsl import (
-    Item,
-    IfAnd,
-    Enchantment,
-    HasItem,
-    chat,
-)
+from pyhtsl import PlayerStat, goto, IfAnd
 
 
-for tier, pick in (
-    (1, 'wooden_pickaxe'),
-    (2, 'stone_pickaxe'),
-    (3, 'iron_pickaxe'),
-    (4, 'golden_pickaxe'),
-    (5, 'diamond_pickaxe'),
-):
-    item = Item(
-        pick,
-        name=f'&bTier {tier} Pickaxe',
-        lore='&6Shiny!\n\n&7Go mine some ores!',
-        enchantments=[Enchantment('efficiency', tier)],
-        hide_enchantments_flag=True,
-    )
-    with IfAnd(HasItem(item)):
-        chat(f'&eYou have a Tier {tier} {item.as_title()}!')
+
+goto('function', 'Test')
+for _ in range(20):
+    with IfAnd():
+        pass
+for i in range(1, 101):
+    PlayerStat('foo').value = i
