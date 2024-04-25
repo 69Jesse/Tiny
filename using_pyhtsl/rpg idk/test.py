1d2ed4db6fdd1eaff8135fa4758cc186d6996d1d
Jesse <jesse@jessejanssen.nl>
2024-04-25 05:14:00 +0200

    Update test.py

--- using_pyhtsl/rpg idk/test.py
+++ using_pyhtsl/rpg idk/test.py
@@ -1,8 +1,19 @@
-from pyhtsl import PlayerStat, goto, IfAnd
+from pyhtsl import *  # You can import everything you need individually if you want
 
+experience = PlayerStat('experience')
+reward = PlayerStat('reward')
+multiplier = PlayerStat('multiplier')
+global_multiplier = GlobalStat('multiplier')
 
-goto('function', name='Some Function')
-with IfAnd():
-    for i in range(11):
-        PlayerStat('foo').value = i
-PlayerStat('bar').value += 1
+experience += reward * multiplier * global_multiplier
+chat(f'&eYour EXP has been updated to &a{experience}&e!')
+
+level = PlayerStat('level')
+EXP_TO_LEVEL_UP = 100  # Python variable, ! not ! a stat
+
+with IfAnd(experience >= EXP_TO_LEVEL_UP):
+    experience -= EXP_TO_LEVEL_UP
+    level += 1
+    chat(f'&eYou leveled up to &dLevel {level}&e!')
+with Else:
+    chat(f'&eOnly &a{EXP_TO_LEVEL_UP - experience} EXP&e left to level up!')
