8fad4cacbba9bc584c05c31e69b005cb4287dc7c
Jesse <jesse@jessejanssen.nl>
2024-04-26 04:22:57 +0200

    Update main.rs

--- rust kloten/mosaics/src/main.rs
+++ rust kloten/mosaics/src/main.rs
@@ -1,9 +1,11 @@
-use image::GenericImageView;
-use image::Pixel;
-use std::fs;
-use std::path::PathBuf;
+use image::{GenericImageView, Pixel};
+use std::{error::Error, fs, path::PathBuf};
 
-const MOSAIC_SIZE: (u32, u32) = (8, 8);
+const CELL_SIZE: (u32, u32) = (16, 16);
+const MAX_GRID_SIZE: (u32, u32) = {
+    let n = 16;
+    (n * CELL_SIZE.0, n * CELL_SIZE.1)
+};
 
 struct Mosaic {
     path: PathBuf,
@@ -17,50 +19,42 @@ impl Mosaic {
         }
     }
 
-    fn create_pixels(&mut self) {
-        let img = match image::open(&self.path) {
-            Ok(img) => img.resize_exact(
-                MOSAIC_SIZE.0,
-                MOSAIC_SIZE.1,
-                image::imageops::FilterType::Nearest,
-            ),
-            Err(e) => {
-                eprintln!("Could not open image: {}", e);
-                return;
-            }
-        };
+    fn create_pixels(&mut self) -> Result<(), Box<dyn Error>> {
+        let img = image::open(&self.path)?.resize_exact(
+            CELL_SIZE.0,
+            CELL_SIZE.1,
+            image::imageops::FilterType::Nearest,
+        );
         for y in 0..img.height() {
             for x in 0..img.width() {
                 let pixel = img.get_pixel(x, y).to_rgb();
                 self.pixels.push(pixel);
-                println!("{:?}", pixel);
             }
         }
+        assert!(self.pixels.len() == (CELL_SIZE.0 * CELL_SIZE.1) as usize);
+        Ok(())
     }
 }
 
-fn get_mosaics() -> Option<Vec<Mosaic>> {
-    let paths = match fs::read_dir("./mosaics/") {
-        Ok(paths) => paths,
-        Err(e) => {
-            eprintln!("Error: {}", e);
-            return None;
-        }
-    };
+fn get_mosaics() -> Result<Vec<Mosaic>, Box<dyn Error>> {
+    let paths = fs::read_dir("./mosaics/")?;
     let mut mosaics = Vec::new();
     for path in paths {
         let path = path.unwrap().path();
         let mut mosaic = Mosaic::new(path);
-        mosaic.create_pixels();
+        mosaic.create_pixels()?;
         mosaics.push(mosaic);
     }
-    Some(mosaics)
+    Ok(mosaics)
 }
 
 fn main() {
     let mosaics = match get_mosaics() {
-        Some(mosaics) => mosaics,
-        None => return,
+        Ok(mosaics) => mosaics,
+        Err(e) => {
+            eprintln!("Could not get mosaics: {}", e);
+            return;
+        }
     };
     for mosaic in mosaics {
         println!("{:?}", mosaic.path);
